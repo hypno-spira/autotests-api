162350cc6bbf7e6ae1463ee8d05d53c281d9a1d4
Alena Kolosova <lucy.drow@yandex.ru>
2025-06-14 20:45:47 +0700

    Добавлена аннотация типов для фикстуры function_user в тесте test_get_user_me

--- tests/test_users.py
+++ tests/test_users.py
@@ -5,6 +5,7 @@ import pytest
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
@@ -26,7 +27,7 @@ def test_create_user(public_users_client: PublicUsersClient):  # Использ�
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_get_user_me(private_users_client: PrivateUsersClient, function_user):
+def test_get_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
     response = private_users_client.get_user_me_api()
     response_data = GetUserResponseSchema.model_validate_json(response.text)
 
