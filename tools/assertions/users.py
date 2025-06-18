889ba69f03f3bf3d783f121ccff95c44c9f63017
Alena Kolosova <lucy.drow@yandex.ru>
2025-06-18 20:35:22 +0700

    Практикуемся в написании API автотестов. Часть 2. Пишем API автотест для получения списка курсов

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,4 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from clients.users.users_schema import UserSchema
 from tools.assertions.base import assert_equal
 
@@ -25,13 +25,13 @@ def assert_user(actual: UserSchema, expected: UserSchema):
     :param expected: Ответ API при создании пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_equal(actual.user.email, expected.user.email, "email")
-    assert_equal(actual.user.last_name, expected.user.last_name, "last_name")
-    assert_equal(actual.user.first_name, expected.user.first_name, "first_name")
-    assert_equal(actual.user.middle_name, expected.user.middle_name, "middle_name")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
 
 
-def assert_get_user_response(get_user_response, create_user_response):
+def assert_get_user_response(get_user_response: GetUserResponseSchema, create_user_response: CreateUserResponseSchema):
     """
     Проверяет, что данные при создании и при запросе пользователя совпадают.
 
@@ -39,4 +39,4 @@ def assert_get_user_response(get_user_response, create_user_response):
     :param create_user_response: Ответ API при создании пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_user(get_user_response, create_user_response)
+    assert_user(get_user_response.user, create_user_response.user)
