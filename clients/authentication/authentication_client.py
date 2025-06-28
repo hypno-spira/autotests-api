40c9263c427fc3ab7c7777698530e820899ed8b7
Alena Kolosova <lucy.drow@yandex.ru>
2025-06-28 20:02:18 +0700

    Практикуемся в интеграции покрытия в API тесты. Настройка и применение на практике инструмента Swagger Coverage Tool

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -2,7 +2,7 @@ import allure
 from httpx import Response
 
 from clients.api_client import APIClient
-# Добавили импорт моделей
+from clients.api_coverage import tracker
 from clients.authentication.authentication_schema import LoginRequestSchema, RefreshRequestSchema, LoginResponseSchema
 from clients.public_http_builder import get_public_http_client
 from tools.routes import APIRoutes
@@ -16,6 +16,8 @@ class AuthenticationClient(APIClient):
     """
 
     @allure.step("Authenticate user")
+    # Добавили сбор покрытия для эндпоинта POST /api/v1/authentication/login
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/login")
     def login_api(self, request: LoginRequestSchema) -> Response:
         """
         Метод выполняет аутентификацию пользователя.
@@ -29,6 +31,8 @@ class AuthenticationClient(APIClient):
         )
 
     @allure.step("Refresh authentication token")
+    # Добавили сбор покрытия для эндпоинта POST /api/v1/authentication/refresh
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/refresh")
     def refresh_api(self, request: RefreshRequestSchema) -> Response:
         """
         Метод обновляет токен авторизации.
